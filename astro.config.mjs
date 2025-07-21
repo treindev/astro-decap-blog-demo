5853dffbd614ca2e68fb751b87ffac49bb154878
trein.dev <thomasbruijs@live.nl>
2025-07-21 20:40:21 +0200

    feat: setup Decap CMS using CDN instead of npm package

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://astro-decap-blog-demo.netlify.app',
 	integrations: [mdx(), sitemap()],
 });
--- src/env.d.ts
+++
@@ -1,8 +0,0 @@
-import type { CMS } from "decap-cms-core";
-
-declare global {
-  interface Window {
-    CMS_MANUAL_INIT?: boolean;
-    CMS?: CMS;
-  }
-}
