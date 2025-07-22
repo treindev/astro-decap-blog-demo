a0d51e1f6eca8216c4a32aee43592c683773ad73
trein.dev <thomasbruijs@live.nl>
2025-07-22 11:26:33 +0200

    feat: add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-decap-blog-demo.netlify.app',
-	integrations: [mdx(), sitemap()],
+  site: 'https://astro-decap-blog-demo.netlify.app',
+  integrations: [mdx(), sitemap()],
+  adapter: netlify(),
 });
